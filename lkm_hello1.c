0741588910d809a3c8bb39df0c7ee830e38b4dd5
Vitor Bueno <vitorcamargo@alunos.utfpr.edu.br>
2018-12-10 22:48:48 -0200

    Update lkm_hello1.c

--- lkm_hello1.c
+++ lkm_hello1.c
@@ -1,28 +1,40 @@
-#include <linux/module.h>       // Needed by all modules
-#include <linux/kernel.h>       // KERN_INFO
-#include <linux/sched.h>        // for_each_process, pr_info
+#include <linux/module.h>
+#include <linux/printk.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/sched/signal.h>
+#include <linux/sched.h>
+#include <linux/memcontrol.h>
 
-void procs_info_print(void) {
-    struct task_struct* task_list;
-    size_t process_counter = 0;
-    
-    for_each_process(task_list) {
-            pr_info("== %s [%d]\n", task_list->comm, task_list->pid);
-            ++process_counter;
+#include <linux/list.h>   
+#include <linux/cred.h>     
+#include <linux/pid.h>
+
+static int __init ex_init(void)
+{
+    struct task_struct *task, *kid_task;
+    size_t process_counter = 0, kid_process_counter = 0;
+
+    for_each_process(task) {
+        kid_process_counter = 0;
+
+        for_each_process(kid_task) {
+            if(task->pid == kid_task->real_parent->pid) { ++kid_process_counter; }
+        }
+
+        pr_info("--PAI (%s) [%d] - qtd filhos: %zu\n", task->comm, task->pid, kid_process_counter);
+        ++process_counter;
     }
-    printk(KERN_INFO "== Number of process: %zu\n", process_counter);
-}
-
-int init_module(void) {
-    printk(KERN_INFO "[ INIT ==\n");
-
-    procs_info_print();
+    printk(KERN_INFO "== Number of process: %zu\n\n\n\n\n", process_counter);
 
     return 0;
 }
 
-void cleanup_module(void) {
-    printk(KERN_INFO "== CLEANUP ]\n");
+static void __exit ex_fini(void) {
+
 }
 
-MODULE_LICENSE("MIT");
+module_init(ex_init);
+module_exit(ex_fini);
+
+MODULE_LICENSE("GPL");
