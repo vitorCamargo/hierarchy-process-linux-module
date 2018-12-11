2a39340d537747b5b7eed82c50ad7852b6a0aacc
Vitor Bueno <vitorcamargo@alunos.utfpr.edu.br>
2018-12-11 19:24:28 -0200

    Update hierarquia_processos.c

--- hierarquia_processos.c
+++ hierarquia_processos.c
@@ -13,12 +13,14 @@ MODULE_AUTHOR("Thais Zoraski && Vitor Bueno");
 MODULE_DESCRIPTION("Módulo do Linux para hierarquia e contagem de processos");
 MODULE_VERSION("1.4.8");
 
+size_t process_counter = 0;
+
 void verificaFilhos(struct task_struct *task, int hierarquia) {
     struct task_struct *filho;
     size_t process_filho_counter = 0;
     int aux = hierarquia;
 
-    printk(KERN_NOTICE "%*s-%ld-> (%s) [%d] [%d]\n", (4*hierarquia), "", task->parent->state, task->comm, task->pid, task->parent->tgid);
+    printk(KERN_NOTICE "%*s--> (%s) [%d]\n", (4*hierarquia), "", task->comm, task->pid);
 
     for_each_process(filho) {
         if(task->pid == filho->parent->tgid) {
@@ -31,19 +33,20 @@ void verificaFilhos(struct task_struct *task, int hierarquia) {
 
 static int __init ex_init(void) {
     struct task_struct *task;
-    size_t process_counter = 0;
-    printk(KERN_NOTICE "--> (COMM) [PID]\n\n");    
+    printk(KERN_NOTICE "--> (COMM) [PID DO PROCESSO]\n\n");
 
     for_each_process(task) {
         if(task->parent->tgid == 0) verificaFilhos(task, 0);
         ++process_counter;
     }
-    printk(KERN_NOTICE "== Number of process: %zu\n\n\n\n\n", process_counter);
+    printk(KERN_NOTICE "== Número total de processos: %zu\n\n\n\n\n", process_counter);
 
     return 0;
 }
 
-static void __exit ex_fini(void) { }
+static void __exit ex_fini(void) {
+    printk(KERN_NOTICE "=== Obrigado por usar, módulo desenvolvido por Thais e Vitor ===");
+}
 
 module_init(ex_init);
 module_exit(ex_fini);
